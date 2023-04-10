e7b9f4331a0522588e6ab030a2f3cff0a6dc89e5
fermer123 <fermer_525@mail.ru>
2023-04-11 00:25:04 +0500

    rolelist start

--- src/components/roleList/RoleList.test.tsx
+++ src/components/roleList/RoleList.test.tsx
@@ -2,6 +2,7 @@ import {render, screen} from '@testing-library/react';
 import RoleFilterSelect, {IRoleFilterSelect} from './RoleList';
 
 describe('<RoleFilterSelect/>', () => {
+  const roles = ['cook', 'driver', 'waiter'];
   const action = jest.fn();
   const props: IRoleFilterSelect = {
     role: '',
@@ -10,9 +11,19 @@ describe('<RoleFilterSelect/>', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
+  test('toMatchSnapshot', () => {
+    render(<RoleFilterSelect {...props} />);
+    const linkElement = screen.getByTestId('select');
+    expect(linkElement).toMatchSnapshot();
+  });
   test('toHaveAtribute', () => {
     render(<RoleFilterSelect {...props} />);
     const linkElement = screen.getByTestId('select');
-    expect(linkElement).toBeEnabled();
+    expect(linkElement).toBeInTheDocument();
+    // eslint-disable-next-line array-callback-return
+    roles.map((e) => {
+      const option = screen.getByText(e);
+      expect(option).toBeInTheDocument();
+    });
   });
 });
